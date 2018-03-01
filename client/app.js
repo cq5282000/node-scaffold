6254b35fcd9d17443b9b42b1402b856a5160b243
chenqu <chenqu@meituan.com>
2018-03-01 18:42:33 +0800

    add

---
+++ client/app.js
@@ -0,0 +1,20 @@
+/*
+ * @Author: chenqu 
+ * @Date: 2018-03-01 17:47:11 
+ * @Last Modified by: chenqu
+ * @Last Modified time: 2018-03-01 18:42:26
+ */
+const React = require('react');
+
+var Application = React.createClass({
+	render: function() {
+		// require("./Application.css");
+		return (
+			<div>
+				<h1>Hello World</h1>
+			</div>
+		);
+	}
+});
+
+module.exports = Application;
