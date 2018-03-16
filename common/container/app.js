757aa0d85373c1de1f6265b0e73f3d09df7946dc
chenqu <chenqu@meituan.com>
2018-03-16 19:54:36 +0800

    add

---
+++ common/container/app.js
@@ -0,0 +1,54 @@
+/*
+ * @Author: chenqu 
+ * @Date: 2018-03-01 17:47:11 
+ * @Last Modified by: chenqu
+ * @Last Modified time: 2018-03-12 20:47:54
+ */
+// const React = require('react');
+
+// const Component = React.createElement('h1', null, 'hello world');
+
+// module.exports = Component;
+
+const waiting = (s) => {
+    return new Promise((resolve) => {
+        setTimeout(function() {
+            resolve();
+        }, s);
+    });
+}
+
+import React, {Component} from 'react';
+
+class Test extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            msg: 'hello world',
+        };
+    }
+
+    async componentWillMount() {
+        console.log('componentWillMount');
+        await waiting(1000);
+        this.setState({
+            msg: 'hi world',
+        });
+    }
+    componentDidMount() {
+        console.log('componentDidMount');
+    }
+    render() {
+        return (
+            <div>
+                <h1>{this.state.msg}</h1>
+            </div>
+        );
+    }
+}
+
+export default () => {
+    return (<Test />);
+}
